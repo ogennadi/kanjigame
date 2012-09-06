fbbfdcec11d1e831b41b6d573e30cd587cd87f58
Oge Nnadi <oge@nnadi.org>
2012-09-06 19:04:14 +0100

    add-kanji-to-db function

--- lib/slurp.rb
+++ lib/slurp.rb
@@ -1,27 +1,32 @@
 class Slurp
   class << self
-    JMDICT_PATH = 'JMdict.txt.trunc' #ftp://ftp.monash.edu.au/pub/nihongo/JMdict.gz
+    JMDICT_PATH = 'JMdict.txt' #ftp://ftp.monash.edu.au/pub/nihongo/JMdict.gz
     N5_PATH = 'vocabn5.htm' # http://www.jlptstudy.net/N5/N5_vocab-list.html
 
     def jmdict
       root = Nokogiri::XML(open(JMDICT_PATH))		
 
       root.xpath('//entry').each do |entry|
-        kanji   = entry.xpath('k_ele/keb').first
-        reading = entry.xpath('r_ele/reb').first
-        writing = kanji.blank? ? reading : kanji
-        meaning = entry.xpath('sense').first.xpath('gloss').first
-        pos     = entry.xpath('sense').first.xpath('pos').first
-        puts "#{writing.content} #{reading.content} (#{pos.children}) #{meaning.content}"
-      end
-    end
+        kanjis   = entry.xpath('k_ele/keb').map(&:content)
+        reading = entry.xpath('r_ele/reb').first.content
+        meaning = entry.xpath('sense').first.xpath('gloss').first.content
+        pos     = entry.xpath('sense').first.xpath('pos').first.children
 
-    def vocabn5
-      root = Nokogiri::HTML(open(N5_PATH))
+        kanjis.each do |kanji|
+          word = Word.find_or_initialize_by_kanji(:kanji => kanji,
+                                              :reading => reading,
+                                              :pos => pos.to_s,
+                                              :meaning => meaning)
 
-      root.css('tr td.kanji:nth-child(3)').each do |td|
-        kanji = td.content
-        print "#{kanji} " unless kanji.blank?
+          characters = word.kanji.split('').map do |char|
+            Character.find_or_create_by_glyph(char)
+          end
+          
+          word.characters = characters
+          word.save!
+
+          puts "#{kanji} #{reading} (#{pos}) #{meaning}"
+        end
       end
     end
   end
